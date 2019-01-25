bf0cad336cd97581a8a68bb8360526b9f7802047
monsieur-aug <hez026@my.utsa.edu>
2019-01-24 20:46:03 -0600

    Create hello world Products resource

---
+++ groceries-service/groceries/src/main/java/com/heb/groceries/resource/ProductsResource.java
@@ -0,0 +1,16 @@
+package com.heb.groceries.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("products")
+public class ProductsResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getProducts() {
+		return "Hello, World!";
+	}
+}
