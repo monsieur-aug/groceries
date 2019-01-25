8367308b68eb758b39eb15f9269817bcfaa31169
monsieur-aug <hez026@my.utsa.edu>
2019-01-24 20:47:34 -0600

    Singularize Product resource

--- groceries-service/groceries/src/main/java/com/heb/groceries/resource/ProductResource.java
+++ groceries-service/groceries/src/main/java/com/heb/groceries/resource/ProductResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("products")
-public class ProductsResource {
+public class ProductResource {
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
