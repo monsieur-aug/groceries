8fbf683f886734aace6ac1d1bf487e9044076503
monsieur-aug <hez026@my.utsa.edu>
2019-01-25 20:02:58 -0600

    Implement unhappy path product builder unit tests

--- groceries-service/groceries/src/test/java/com/heb/groceries/model/ProductBuilderTest.java
+++ groceries-service/groceries/src/test/java/com/heb/groceries/model/ProductBuilderTest.java
@@ -5,7 +5,11 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.heb.groceries.error.Error;
 
 public class ProductBuilderTest {
 
@@ -18,6 +22,9 @@ public class ProductBuilderTest {
 	private final String		validUnit			= "Each";
 	private final int			validXFor			= 1;
 	private final BigDecimal	validCost			= new BigDecimal("1.20");
+
+	@Rule
+	public ExpectedException	expectedException	= ExpectedException.none();
 
 	@Test
 	public void testProductBuilderBuildsProductGivenValidValues() {
@@ -38,4 +45,113 @@ public class ProductBuilderTest {
 		assertEquals("The xFor should be the same as the one specified", this.validXFor, testProduct.getXFor());
 		assertEquals("The cost should be the same as the one specified", this.validCost, testProduct.getCost());
 	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetIdDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutId = new ProductBuilder();
+		testBuilderWithoutId.description(this.validDescription).lastSold(this.validLastSold).shelfLifeDays(this.validShelfLifeDays)
+						.department(this.validDepartment).price(this.validPrice).unit(this.validUnit).xFor(this.validXFor).cost(this.validCost);
+
+		testBuilderWithoutId.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetDescriptionDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutDescription = new ProductBuilder();
+		testBuilderWithoutDescription.id(this.validId).lastSold(this.validLastSold).shelfLifeDays(this.validShelfLifeDays).department(this.validDepartment)
+						.price(this.validPrice).unit(this.validUnit).xFor(this.validXFor).cost(this.validCost);
+
+		testBuilderWithoutDescription.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetLastSoldDateDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutLastSoldDate = new ProductBuilder();
+		testBuilderWithoutLastSoldDate.id(this.validId).description(this.validDescription).shelfLifeDays(this.validShelfLifeDays)
+						.department(this.validDepartment).price(this.validPrice).unit(this.validUnit).xFor(this.validXFor).cost(this.validCost);
+
+		testBuilderWithoutLastSoldDate.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetShelfLifeDaysDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutShelfLifeDays = new ProductBuilder();
+		testBuilderWithoutShelfLifeDays.id(this.validId).description(this.validDescription).lastSold(this.validLastSold).department(this.validDepartment)
+						.price(this.validPrice).unit(this.validUnit).xFor(this.validXFor).cost(this.validCost);
+
+		testBuilderWithoutShelfLifeDays.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetDepartmentDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutDepartment = new ProductBuilder();
+		testBuilderWithoutDepartment.id(this.validId).description(this.validDescription).lastSold(this.validLastSold).shelfLifeDays(this.validShelfLifeDays)
+						.price(this.validPrice).unit(this.validUnit).xFor(this.validXFor).cost(this.validCost);
+
+		testBuilderWithoutDepartment.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetPriceDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutPrice = new ProductBuilder();
+		testBuilderWithoutPrice.id(this.validId).description(this.validDescription).lastSold(this.validLastSold).shelfLifeDays(this.validShelfLifeDays)
+						.department(this.validDepartment).unit(this.validUnit).xFor(this.validXFor).cost(this.validCost);
+
+		testBuilderWithoutPrice.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetUnitDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutUnit = new ProductBuilder();
+		testBuilderWithoutUnit.id(this.validId).description(this.validDescription).lastSold(this.validLastSold).shelfLifeDays(this.validShelfLifeDays)
+						.department(this.validDepartment).price(this.validPrice).xFor(this.validXFor).cost(this.validCost);
+
+		testBuilderWithoutUnit.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetXForDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutXFor = new ProductBuilder();
+		testBuilderWithoutXFor.id(this.validId).description(this.validDescription).lastSold(this.validLastSold).shelfLifeDays(this.validShelfLifeDays)
+						.department(this.validDepartment).price(this.validPrice).unit(this.validUnit).cost(this.validCost);
+
+		testBuilderWithoutXFor.build();
+	}
+
+	@Test
+	public void testProductBuilderThrowsIllegalStateExceptionGivenUnsetCostDuringBuildAttempt() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(Error.PRODUCT_BUILDER_INCOMPLETE.toString());
+
+		final ProductBuilder testBuilderWithoutCost = new ProductBuilder();
+		testBuilderWithoutCost.id(this.validId).description(this.validDescription).lastSold(this.validLastSold).shelfLifeDays(this.validShelfLifeDays)
+						.department(this.validDepartment).price(this.validPrice).unit(this.validUnit).xFor(this.validXFor);
+
+		testBuilderWithoutCost.build();
+	}
+
 }
