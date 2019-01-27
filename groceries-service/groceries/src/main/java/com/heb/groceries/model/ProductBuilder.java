3c10f16d6a41980bda4ef1665975e417642b82e1
monsieur-aug <hez026@my.utsa.edu>
2019-01-26 22:04:20 -0600

    Consolidate xxIsSet() verification methods with isSet()

--- groceries-service/groceries/src/main/java/com/heb/groceries/model/ProductBuilder.java
+++ groceries-service/groceries/src/main/java/com/heb/groceries/model/ProductBuilder.java
@@ -14,8 +14,9 @@ public class ProductBuilder {
 	}
 
 	public Product build() {
-		if (idIsSet() && descriptionIsSet() && lastSoldIsSet() && shelfLifeDaysIsSet() && departmentIsSet() && priceIsSet() && unitIsSet() && xForIsSet()
-						&& costIsSet()) {
+		if (isSet(this.product.getId()) && isSet(this.product.getDescription()) && isSet(this.product.getLastSold()) && isSet(this.product.getShelfLifeDays())
+						&& isSet(this.product.getDepartment()) && isSet(this.product.getPrice()) && isSet(this.product.getUnit())
+						&& isSet(this.product.getXFor()) && isSet(this.product.getCost())) {
 			return this.product;
 		}
 
@@ -74,75 +75,6 @@ public class ProductBuilder {
 		this.product.setCost(cost);
 
 		return this;
-	}
-
-	private boolean idIsSet() {
-		/*
-		 * The ID value is considered set if the current ID value passed the input
-		 * validation of the ID setter. If it didn't pass validation, then it wasn't
-		 * set.
-		 */
-		try {
-			this.product.setId(this.product.getId());
-		} catch (IllegalArgumentException iae) {
-			return false;
-		}
-
-		return true;
-	}
-
-	private boolean descriptionIsSet() {
-		return isSet(this.product.getDescription());
-	}
-
-	private boolean lastSoldIsSet() {
-		return isSet(this.product.getLastSold());
-	}
-
-	private boolean shelfLifeDaysIsSet() {
-		/*
-		 * The shelf life days value is considered set if the current shelf life days
-		 * value passed the input validation of the shelf life days setter. If it didn't
-		 * pass validation, then it wasn't set.
-		 */
-		try {
-			this.product.setShelfLifeDays(this.product.getShelfLifeDays());
-		} catch (IllegalArgumentException iae) {
-			return false;
-		}
-
-		return true;
-	}
-
-	private boolean departmentIsSet() {
-		return isSet(this.product.getDepartment());
-	}
-
-	private boolean priceIsSet() {
-		return isSet(this.product.getPrice());
-	}
-
-	private boolean unitIsSet() {
-		return isSet(this.product.getUnit());
-	}
-
-	private boolean xForIsSet() {
-		/*
-		 * The xFor value is considered set if the current xFor value passed the input
-		 * validation of the xFor setter. If it didn't pass validation, then it wasn't
-		 * set.
-		 */
-		try {
-			this.product.setXFor(this.product.getXFor());
-		} catch (IllegalArgumentException iae) {
-			return false;
-		}
-
-		return true;
-	}
-
-	private boolean costIsSet() {
-		return isSet(this.product.getCost());
 	}
 
 	private boolean isSet(Object object) {
