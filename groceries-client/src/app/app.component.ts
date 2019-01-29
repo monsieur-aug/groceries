63be940287e8a52665dd66bcd07aa01a0dcbd2fb
monsieur-aug <hez026@my.utsa.edu>
2019-01-29 17:22:02 -0600

    Move search results component to be a child of search component

--- groceries-client/src/app/app.component.ts
+++ groceries-client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component } from '@angular/core';
-import { Product } from './search-results/product.model';
-import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,50 +7,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
 
-  retrievedProducts : Product[];
-
-  constructor(private http : HttpClient) {
-    let apples : any = {
-      id: 321654,
-      description: 'apples',
-      lastSoldDate: '2017-9-7',
-      shelfLifeDays: 7,
-      department: 'Produce',
-      price: 1.49,
-      unit: 'lb',
-      xFor: 1,
-      cost: 0.2
-    };
-
-    let headacheMedicine : any = {
-      id: 9000001,
-      description: 'headache medicine',
-      lastSoldDate: '2017-9-20',
-      shelfLifeDays: 365,
-      department: 'Pharmacy',
-      price: 5.89,
-      unit: 'Each',
-      xFor: 1,
-      cost: 1.9
-    };
-
-    let cereal : any = {
-      id: 119290,
-      description: 'cereal',
-      lastSoldDate: '2017-9-18',
-      shelfLifeDays: 40,
-      department: 'Grocery',
-      price: 3.19,
-      unit: 'Each',
-      xFor: 1,
-      cost: 0.19
-    }; 
-
-    this.retrievedProducts = [ apples, headacheMedicine, cereal ];
+  constructor() {
+    
   }
 
   ngOnInit() {
-    let httpObservable = this.http.get('http://localhost:8080/groceries/api/products');
-    httpObservable.subscribe(() => console.log('Got a response!'));
+    
   }
+
 }
--- groceries-client/src/app/search/search.component.ts
+++ groceries-client/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../search-results/product.model';
 
 @Component({
   selector: 'app-search',
@@ -14,12 +16,55 @@ export class SearchComponent implements OnInit {
   departmentField : string;
   priceField : string;
 
-  constructor() { }
+  retrievedProducts : Product[];
+
+  constructor(private http : HttpClient) {
+
+  }
 
   ngOnInit() {
   }
 
   toggleMoreSearchOptions() {
     this.isMoreOptionsExpanded = !this.isMoreOptionsExpanded;
+  }
+
+  buildSearchParams() : URLSearchParams {
+    let searchParams = new URLSearchParams();
+
+    if (this.descriptionField) {
+      searchParams.append('descriptionContains', this.descriptionField);
+    }
+
+    if (this.departmentField) {
+      searchParams.append('department', this.departmentField);
+    }
+
+    if (this.priceField) {
+      searchParams.append('priceMin', this.priceField);
+      searchParams.append('priceMax', this.priceField);
+    }
+
+    return searchParams;
+  }
+
+  findProducts() {
+    let searchParams : URLSearchParams = this.buildSearchParams();
+    let searchUrl = new URL('http://localhost:8080/groceries/api/products'.concat('?', searchParams.toString()));
+
+    console.log('contacting ' + searchUrl.toString());
+    this.http.get(searchUrl.toString()).subscribe((response) => this.handleResponse(response));
+  }
+
+  handleResponse(response : any) {
+    console.log('handling response...');
+
+    if (Array.isArray(response)) {
+      this.retrievedProducts = response;
+    } else if (response.id) {
+      this.retrievedProducts = [ response ];
+    }
+
+    console.log(this.retrievedProducts);
   }
 }
