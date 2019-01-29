8e095721ee2750e2cc43c8b4f317be26af15a2b3
monsieur-aug <hez026@my.utsa.edu>
2019-01-29 09:59:07 -0600

    Create mock product data in the root component

--- groceries-client/src/app/app.component.ts
+++ groceries-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './search-results/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'H-E-B Grocery Search App';
+
+  retrievedProducts : Product[];
+
+  constructor() {
+    let apples : any = {
+      id: 321654,
+      description: 'apples',
+      lastSoldDate: '2017-9-7',
+      shelfLifeDays: 7,
+      department: 'Produce',
+      price: 1.49,
+      unit: 'lb',
+      xFor: 1,
+      cost: 0.2
+    };
+
+    let headacheMedicine : any = {
+      id: 9000001,
+      description: 'headache medicine',
+      lastSoldDate: '2017-9-20',
+      shelfLifeDays: 365,
+      department: 'Pharmacy',
+      price: 5.89,
+      unit: 'Each',
+      xFor: 1,
+      cost: 1.9
+    };
+
+    let cereal : any = {
+      id: 119290,
+      description: 'cereal',
+      lastSoldDate: '2017-9-18',
+      shelfLifeDays: 40,
+      department: 'Grocery',
+      price: 3.19,
+      unit: 'Each',
+      xFor: 1,
+      cost: 0.19
+    }; 
+
+    this.retrievedProducts = [ apples, headacheMedicine, cereal ];
+  }
 }
