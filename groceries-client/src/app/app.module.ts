402774afda5e238a6857847b668ebf18c43474f9
monsieur-aug <hez026@my.utsa.edu>
2019-01-28 18:50:29 -0600

    Add placeholder components for header, footer, search, and search results

--- groceries-client/src/app/app.module.ts
+++ groceries-client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule
