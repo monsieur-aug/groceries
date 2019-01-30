f8665e60a0031a3732f73806c507cf3374257d74
monsieur-aug <hez026@my.utsa.edu>
2019-01-29 19:34:18 -0600

    Add id query param

--- groceries-service/groceries/src/main/java/com/heb/groceries/resource/query/QueryParam.java
+++ groceries-service/groceries/src/main/java/com/heb/groceries/resource/query/QueryParam.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum QueryParam {
+	ID("id"),
 	DESCRIPTION("descriptionContains"),
 	DEPARTMENT("department"),
 	SHELF_LIFE_DAYS_MIN("shelfLifeMin"),
